306d5591151df35d3d45b0123bce7440fd93b444
Jakob <jakob-151@web.de>
2021-07-15 00:14:59 +0200

    dotted line are now added automativly

--- visualization/pub.js
+++ visualization/pub.js
@@ -8,11 +8,12 @@ const width = document.getElementById('graph').clientWidth;
 const height = document.getElementById('graph').clientHeight;
 const center = Math.min(width/2, height/2)
 
+var start_day = 0;
 // needs to be extracted from meta-data
-var max_day = 455;
+var end_day = 456;
 
 function day_to_radians(day) {
-  let result = angle_gap / 2 + (360 - angle_gap) * day / max_day; // relativ day in degrees with the angle_gap subtracted. Day 0 will be mapped to angle_gap/2 aka the start of the circle.
+  let result = angle_gap / 2 + (360 - angle_gap) * day / end_day; // relativ day in degrees with the angle_gap subtracted. Day 0 will be mapped to angle_gap/2 aka the start of the circle.
   return result * (Math.PI / 180); // converting to radians
 }
 
@@ -68,9 +69,30 @@ const g = vis.append("g").attr("transform", `translate(${width/2}, ${height/2})`
 
 $.getJSON("../data/main.json", function(data) {
   // store length of data - equal to the number of different words
-  n = 25 // hardcoded um Sachen zu testen :D max: 148 bzw data.length
+  n = 60 // hardcoded um Sachen zu testen :D max: 148 bzw data.length
   line_width = (center - inner_offset)*0.8/n
   line_gap = line_width*0.25
+  skip_rings = Math.ceil(n/15) // number of rings where no dotted line should be drawn
+  let j = 1; // Variable to ensure we get a maximum of 100 lines
+  
+  // dotted lines
+  for (let i = start_day; i <= end_day; i++) {
+    if(end_day/j < 100) {
+      let angle = (((360-angle_gap) * (i-0.5)/end_day + angle_gap/2)*(Math.PI)/180)
+      g.append("line")
+        .attr("x1", Math.sin(angle)*(inner_offset+2.375*line_width+2*line_gap))     // x position of the first end of the line
+        .attr("y1", -Math.cos(angle)*(inner_offset+2.375*line_width+2*line_gap))      // y position of the first end of the line
+        .attr("x2", Math.sin(angle)*center)     // x position of the second end of the line
+        .attr("y2", -Math.cos(angle)*center)
+        .attr("stroke-dasharray", `${line_gap}, ${(line_width+line_gap)*skip_rings-line_gap}`)
+        .attr("class", "graph_line");
+      j = 1;
+    } else {
+      j++;
+    }
+  }
+  
+  // circle bars
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < data[i].days.length; j++) {
       // to satisfy d3 syntax this has to be done
